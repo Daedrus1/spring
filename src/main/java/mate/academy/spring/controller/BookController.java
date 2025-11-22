0852d3a801a67720fbdf04c9ac9c9badfec692dd
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-11-22 23:03:12 +0100

    Add Liquibase migrations, JPA repository, and soft delete for Book entity

--- src/main/java/mate/academy/spring/controller/BookController.java
+++ src/main/java/mate/academy/spring/controller/BookController.java
@@ -5,7 +5,14 @@ import lombok.RequiredArgsConstructor;
 import mate.academy.spring.dto.BookDto;
 import mate.academy.spring.dto.CreateBookRequestDto;
 import mate.academy.spring.service.BookService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/books")
